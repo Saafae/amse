a19f9b3adee4a1e73eadce75a82c10c866b371ef
Safae <72393073+Saafae@users.noreply.github.com>
2023-02-10 17:53:41 +0100

    exo 5

--- DartExo/test.dart
+++ DartExo/test.dart
@@ -1,13 +1,16 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Entrez un nombre: ");
+  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+  List c = [];
 
-  int num = int.parse(stdin.readLineSync()!);
-  print('La liste des diviseurs: ');
-  for (int i = 1; i <= num; i++) {
-    if (num % i == 0) {
-      print(i);
+  for (int i in a) {
+    for (int j in b) {
+      if (i == j) {
+        c.add(i);
+      }
     }
   }
+  print(c);
 }
