32eb068dfd0d215f882cf374518bb29eefa098f4
Safae <72393073+Saafae@users.noreply.github.com>
2023-02-10 17:46:41 +0100

    exo 2

--- DartExo/test.dart
+++ DartExo/test.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Your name? ");
-  String? name = stdin.readLineSync();
-  print("Hi, $name! Your age?");
+  stdout.write("Entrez un nombre: ");
+  int number = int.parse(stdin.readLineSync()!);
 
-  String? age = stdin.readLineSync();
-  int yearsleft = 100 - int.parse(age?.toString() ?? '0');
-
-  print("$name, You have $yearsleft years to be 100 !!");
+  if (number % 2 == 0) {
+    print("Pair");
+  } else {
+    print("Impair");
+  }
 }
