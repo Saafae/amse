f74d12bbfa8ebff2293ad4fd7e92e9da2ee1c375
Safae <72393073+Saafae@users.noreply.github.com>
2023-02-20 17:19:46 +0100

    exo 2a Rotation + mirroir + scale

--- TP2/flutter_app_tp2/lib/main.dart
+++ TP2/flutter_app_tp2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Image aléatoire'),
+      home: const MyHomePage(title: 'Transformer une image'),
     );
   }
 }
@@ -27,14 +28,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _currentRotateXValue = 0;
+  double _currentRotateZValue = 0;
+  double _scale = 1.0;
+  double _currentScaleValue = 1;
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Image(
-        image: NetworkImage('https://picsum.photos/512/1024'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateX(_currentRotateXValue)
+                ..rotateZ(_currentRotateZValue)
+                ..rotateY(isChecked ? -pi : 0)
+                ..scale(_currentScaleValue),
+              child: Image.asset(
+                'image.jpg',
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('RotateX:'),
+              Slider(
+                value: _currentRotateXValue,
+                max: 2 * pi,
+                onChanged: (double value) {
+                  setState(() {
+                    _currentRotateXValue = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('RotateZ:'),
+              Slider(
+                value: _currentRotateZValue,
+                max: 2 * pi,
+                onChanged: (double value) {
+                  setState(() {
+                    _currentRotateZValue = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Mirror:'),
+              Checkbox(
+                checkColor: Colors.white,
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value!;
+                  });
+                },
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Scale:'),
+              Slider(
+                value: _currentScaleValue,
+                max: 10,
+                min: 0,
+                onChanged: (double value) {
+                  setState(() {
+                    _currentScaleValue = value;
+                  });
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
