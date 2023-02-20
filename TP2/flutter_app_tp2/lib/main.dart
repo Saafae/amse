25905bc63979e390a86770d2b6b1e5a6227d5ddb
Safae <72393073+Saafae@users.noreply.github.com>
2023-02-20 21:18:13 +0100

    exo 2 animation

--- TP2/flutter_app_tp2/lib/main.dart
+++ TP2/flutter_app_tp2/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -30,9 +32,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double _currentRotateXValue = 0;
   double _currentRotateZValue = 0;
-  double _scale = 1.0;
   double _currentScaleValue = 1;
   bool isChecked = false;
+  bool isAnimating = false;
 
   @override
   Widget build(BuildContext context) {
@@ -109,8 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const Text('Scale:'),
               Slider(
                 value: _currentScaleValue,
-                max: 10,
-                min: 0,
+                max: 2,
                 onChanged: (double value) {
                   setState(() {
                     _currentScaleValue = value;
@@ -118,9 +119,77 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               ),
             ],
-          )
+          ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isAnimating = !isAnimating;
+          });
+
+          if (isAnimating) {
+            const d = Duration(milliseconds: 50);
+            Timer.periodic(d, animate);
+          }
+        },
+        child:
+            isAnimating ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
+      ),
     );
+  }
+
+  bool _animatedScale = true;
+  bool _animatedRotX = true;
+  bool _animatedRotZ = true;
+
+  void animate(Timer t) {
+    setState(() {
+      if (_animatedScale) {
+        _currentScaleValue += 0.1;
+        if (_currentScaleValue >= 2.0) {
+          _currentScaleValue = 2.0;
+          _animatedScale = false;
+        }
+      } else {
+        _currentScaleValue -= 0.1;
+        if (_currentScaleValue <= 0.0) {
+          _currentScaleValue = 0.0;
+          _animatedScale = true;
+        }
+      }
+
+      if (_animatedRotX) {
+        _currentRotateXValue += 0.1;
+        if (_currentRotateXValue >= 2 * pi) {
+          _currentRotateXValue = 2 * pi;
+          _animatedRotX = false;
+        }
+      } else {
+        _currentRotateXValue -= 0.1;
+        if (_currentRotateXValue <= 0.0) {
+          _currentRotateXValue = 0.0;
+          _animatedRotX = true;
+        }
+      }
+
+      if (_animatedRotZ) {
+        _currentRotateZValue += 0.2;
+        if (_currentRotateZValue >= 2 * pi) {
+          _currentRotateZValue = 2 * pi;
+          _animatedRotZ = false;
+        }
+      } else {
+        _currentRotateZValue -= 0.2;
+        if (_currentRotateZValue <= 0.0) {
+          _currentRotateZValue = 0.0;
+          _animatedRotZ = true;
+        }
+      }
+    });
+
+    if (!isAnimating) {
+      t.cancel();
+    }
   }
 }
